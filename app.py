f88477488a95eecd9fc13280da69cdc26ecbf781
gauravBangade <bangadegaurav@gmail.com>
2023-10-26 23:15:42 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,152 @@
+import streamlit as st
+from auth import *
+from google.oauth2 import id_token
+from google.auth.transport import requests
+from user_handaling import *
+from streamlit_option_menu import option_menu
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from HMLTemplats import css, bot_template, user_template
+
+
+def get_pdf_text(pdf_doc):
+    text = ""
+    for pdf in pdf_doc:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="chat with pdf", page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+
+    cookie_manager = stx.CookieManager()
+    cookie_value = cookie_manager.get("username")
+
+    with st.sidebar:
+        if cookie_value is not None:
+            if st.button("logout"):
+                logout()
+            st.write("Welcome", cookie_value)
+
+        selected = option_menu(
+            menu_title="Inquire.AI",
+            options=["Chat", "login/signup", "Learn more", "Contact", "History"],
+            icons=["chat-left-fill", "person-lines-fill",
+                   "arrow-up-right-square", "bi bi-telephone", "clock-history"],
+            menu_icon="Home",
+            default_index=0
+        )
+
+    if selected == "Chat":
+        st.subheader("upload your notes PDF 📄")
+        pdf_doc = st.file_uploader("up-lode", accept_multiple_files=True)
+        if st.button("process"):
+            if cookie_value is not None:
+                with st.spinner("processing"):
+                    # get pdf text
+                    raw_text = get_pdf_text(pdf_doc)
+                    # st.write(raw_text)
+                    # get text chunks
+                    text_chunks = get_text_chunks(raw_text)
+                    # st.write(text_chunks)
+                    # create embeddings
+                    vectorstore = get_vectorstore(text_chunks)
+                    # create conversation chain
+                    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+
+                    st.session_state.conversation = get_conversation_chain(vectorstore)
+            else:
+                st.warning("Please login")
+        if cookie_value is not None:
+            user_question = st.text_input("Ask inquireAI ")
+            if user_question:
+                handle_userinput(user_question)
+    if selected == "login/signup":
+        with st.container():
+            st.subheader("login")
+            username = st.text_input("Enter your email")
+            password = st.text_input("password", type="password")
+            if st.button("login"):
+                login(username, password)
+            st.write("---")
+        with st.container():
+            name = st.text_input("chose your name")
+            email = st.text_input("email")
+            nwe_password = st.text_input("code", type="password")
+            if st.button("signup"):
+                signup(name, nwe_password, email)
+
+    if selected == "Learn more":
+        st.write("this is learn more page ")
+
+    if selected == "Contact":
+        st.write("this is contact form")
+
+    if selected == "History":
+        st.subheader("Check  history")
+
+
+if __name__ == '__main__':
+    main()
---
+++ user_handaling.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from pymongo import MongoClient
+from passlib.hash import pbkdf2_sha256
+import re
+import extra_streamlit_components as stx
+
+cookie_manager = stx.CookieManager()
+client = MongoClient("mongodb://localhost:27017/")
+db = client["user_database"]
+users = db["users"]
+
+
+def is_valid_email(email):
+    email_pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    return re.match(email_pattern, email) is not None
+
+
+def logout():
+    cookie_manager.delete("username")
+
+
+def login(email, password):
+    user_data = users.find_one({"email": email})
+
+    if user_data and pbkdf2_sha256.verify(password, user_data["password"]):
+        uname = user_data["username"]
+        cookie_manager.set("username", "uname")
+        st.success("Login successful! Welcome ")
+        st.write("Welcome ", uname)
+    else:
+        st.error("Login failed. Please check your username and password.")
+        st.write("try signing up below")
+
+
+def signup(username, password, email):
+    # Hash the password before storing it
+    hashed_password = pbkdf2_sha256.hash(password)
+    # email validation
+    if is_valid_email(email):
+        # Check if the username already exists
+        if users.find_one({"email": email}):
+            st.error("User with this email already exists. Please choose a different one.")
+        else:
+            user_data = {"username": username, "password": hashed_password, "email": email}
+            users.insert_one(user_data)
+            st.success("Signup successful! You can now log in.")
+    else:
+        st.warning("invalid email")
